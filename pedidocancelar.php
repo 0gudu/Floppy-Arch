39781b2b35e6be1eacac59f4e081bf40eea42d45
0gudu <gugag3rr@gmail.com>
2023-06-15 09:29:39 -0300

    pedidos e cancelarlos

---
+++ pedidocancelar.php
@@ -0,0 +1,27 @@
+<?php
+    include("conecta.php");
+    $id = $_GET["id"];
+
+    $comando = $pdo->prepare("SELECT * FROM pedidos WHERE id_pedido = :id");
+    $comando->bindParam(':id', $id);
+    $comando->execute();
+
+    $resultado = $comando->execute();
+
+    while( $linhas = $comando->fetch()) 
+        {
+            $num = $linhas["numero"];
+            $user = $linhas["usuario"];
+        }
+
+    $comando = $pdo->prepare("DELETE FROM pedidos WHERE pedidos.id_pedido = :id");
+    $comando->bindParam(':id', $id);
+    $comando->execute();    
+
+    $comando = $pdo->prepare("DELETE FROM carrinho WHERE usuario = :user AND pedido = :pedido");
+    $comando->bindParam(':user', $user);
+    $comando->bindParam(':pedido', $num);
+    $comando->execute();
+
+    header("location: verpedidos.php")
+?>
