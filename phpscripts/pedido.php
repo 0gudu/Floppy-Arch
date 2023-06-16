f42d87c81f25cc7337106071b26f515f329dff4c
0gudu <gugag3rr@gmail.com>
2023-06-15 22:27:22 -0300

    arrmações

---
+++ phpscripts/pedido.php
@@ -0,0 +1,29 @@
+<?php 
+    include("../includes/conecta.php");
+    session_start();
+
+    $_SESSION['user'];
+    $comando = $pdo->prepare("SELECT MAX(pedido) FROM carrinho;");
+    $comando->execute();
+    
+    $resultado = $comando->fetchColumn();
+
+    $cu = $resultado + 1;
+
+    $comando = $pdo->prepare("UPDATE carrinho
+    SET pedido = :pedido
+    WHERE usuario = :usuario and pedido = 0;");
+
+    $comando->bindParam(':pedido', $cu);
+    $comando->bindParam(':usuario', $_SESSION['user']);
+    $comando->execute();
+    
+    $comando = $pdo->prepare("UPDATE carrinho
+    SET pedido = :pedido
+    WHERE usuario = :usuario and pedido = 0;");
+
+    $comando->bindParam(':pedido', $cu);
+    $comando->bindParam(':usuario', $_SESSION['user']);
+    $comando->execute();
+   header("location: pedidos.php");
+?>
