b64eb515242e07db9b82ff69eb799a22e860c95f
Zuquetti <matheuszuquettipereira2020@gmail.com.br>
2023-06-28 12:33:43 -0300

    enviou sldfghsjdh

--- phpscripts/adicionarprodutos.php
+++ phpscripts/adicionarprodutos.php
@@ -1,14 +1,24 @@
 <?php
-    include ("../includes/conecta.php");
+include("../includes/conecta.php");
 
-    $nome = $_POST["nome"];
-    $email = $_POST["email"];
-    $endereco = $_POST["endereco"];
-    $senha = $_POST["senha"];
-    $telefone = $_POST["telefone"];
+$nome = $_POST["nome"];
+$foto = $_POST["foto"];
+$descricao = $_POST["descricao"];
 
-    $comando = $pdo->prepare("INSERT INTO pessoas VALUES('$email','$nome','$endereco', '$senha',$telefone,0)");
-    $resultado = $comando->execute();
-    //para voltar ao formulário//
+$comando = $pdo->prepare("INSERT INTO produtos (nome, foto, descricao) VALUES (:nome, :foto, :descricao)");
+$comando->bindParam(":nome", $nome);
+$comando->bindParam(":foto", $foto);
+$comando->bindParam(":descricao", $descricao);
+
+$resultado = $comando->execute();
+
+// Verifica se a inserção foi bem-sucedida
+if ($resultado) {
+    // Redireciona para a página de adicionar produtos
     header("Location: ../pages/addprodutos.php");
+    exit(); // Encerra o script após o redirecionamento
+} else {
+    // Trata algum erro na inserção
+    echo "Ocorreu um erro ao adicionar o produto.";
+}
 ?>
